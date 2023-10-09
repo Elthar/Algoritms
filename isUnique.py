cc0733326aa0e50ceb391322bd80dc03877d86f9
Elthar <100107294+Elthar@users.noreply.github.com>
2023-10-09 10:32:52 +0600

    Update isUnique.py
    Solved isUnique probl;em in OOP style

--- isUnique.py
+++ isUnique.py
@@ -1,9 +1,30 @@
-def is_unique(s):
-    l = len(s)
-    for i in range(l):
-        for j in range(i+1, l):
-            if s[i] == s[j]:
-                return False 
-    return True
+class Solution:
+    def __init__(self, input_string):
+        self.input_string = input_string
 
-print(is_unique([1,2,3,4,2]))
+    def is_unique(self):
+        l = len(self.input_string)
+        for i in range(l):
+            for j in range(i+1, l):
+                if self.input_string[i] == self.input_string[j]:
+                    return False 
+        return True
+
+my_list = []
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))  
+c = int(input("Enter third number: "))  
+d = int(input("Enter fourth number: "))
+
+my_list.append(a)
+my_list.append(b)
+my_list.append(c)
+my_list.append(d)
+
+unique_checker = Solution(my_list)
+unique = unique_checker.is_unique()
+
+if unique:
+    print(f"{my_list} is unique")
+else:
+    print(f"{my_list} is not unique")
