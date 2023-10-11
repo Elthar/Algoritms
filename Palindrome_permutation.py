29039c15f5bc781f448e2eb3b8ee8d24d5a780cd
Elthar <100107294+Elthar@users.noreply.github.com>
2023-10-11 07:06:36 +0600

    1. Palindrome_permutation.py
    Rewrote this code: make it in OOP style and added comments

--- Palindrome_permutation.py
+++ Palindrome_permutation.py
@@ -1,25 +1,34 @@
-def is_permutation_of_palindrome(x):
-    # remove spaces and convert strings to lowercase
-    x = x.replace(" ", "").lower()
+# In this problem we check if it's possible to obtain a mirrored string by simply rearranging the letters. "
+# If so, return - True, if not, return - False
+class Solution:
+    def __init__(self, str1):
+        self.str1 = str1
 
-    char_count = {}
+    def is_permutation_of_palindrome(self):
+        # remove spaces and convert strings to lowercase
+        self.str1 = self.str1.replace(" ", "").lower()
+        char_count = {}
 
-    for i in x:
-        if i.isalpha():
-            if i in char_count:
-                char_count[i] += 1
-            else:
-                char_count[i] = 1
+        # Check if the character is an alphabet letter.
+        for i in self.str1:
+            if i.isalpha():
+                if i in char_count:
+                    char_count[i] += 1
+                else:
+                    char_count[i] = 1
 
-    odd_count = 0
+        odd_count = 0
+        
+        # Check if the count is odd.
+        for j in char_count.values():
+            if j % 2 != 0:
+                odd_count += 1
 
-    for j in char_count.values():
-        if j % 2 != 0:
-            odd_count += 1
+        return odd_count <= 1
 
-    return odd_count <= 1
-
-a = str(input())
-print(is_permutation_of_palindrome(a))
+# Example usage
+str1 = str(input('Enter string: '))
+result = Solution(str1)
+print(result.is_permutation_of_palindrome())
 
 
