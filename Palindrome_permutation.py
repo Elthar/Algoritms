f6677bc31345ff8e69179f0fd8153fffa8a4982b
Elthar <100107294+Elthar@users.noreply.github.com>
2023-10-15 09:33:52 +0600

    1. new Palindrome_permutation.py

--- Palindrome_permutation.py
+++ Palindrome_permutation.py
@@ -1,16 +1,16 @@
 # In this problem we check if it's possible to obtain a mirrored string by simply rearranging the letters. "
 # If so, return - True, if not, return - False
 class Solution:
-    def __init__(self, str1):
-        self.str1 = str1
+    def __init__(self, my_string):
+        self.my_string = my_string
 
     def is_permutation_of_palindrome(self):
         # remove spaces and convert strings to lowercase
-        self.str1 = self.str1.replace(" ", "").lower()
+        self.my_string = self.my_string.replace(" ", "").lower()
         char_count = {}
 
         # Check if the character is an alphabet letter.
-        for i in self.str1:
+        for i in self.my_string:
             if i.isalpha():
                 if i in char_count:
                     char_count[i] += 1
@@ -27,8 +27,11 @@ class Solution:
         return odd_count <= 1
 
 # Example usage
-str1 = str(input('Enter string: '))
-result = Solution(str1)
+my_string = 'Tact Coa'
+result = Solution(my_string)
 print(result.is_permutation_of_palindrome())
+
+
+
 
 
