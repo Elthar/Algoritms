ff70bfe959f351c47e38e07966b2f16f0e820c50
Elthar <100107294+Elthar@users.noreply.github.com>
2023-10-15 09:41:48 +0600

    3. new is_permutation.py

--- is_permutation.py
+++ is_permutation.py
@@ -1,29 +1,32 @@
-class Solution:
+# In this class we check if second string is a permutation of first string
+class PermutationChecker:
     # initializing variabless
-    def __init__(self, str1, str2):
-        self.str1 = str1
-        self.str2 = str2
+    def __init__(self, first_string, second_string):
+        self.first_string = first_string
+        self.second_string = second_string
 
     def is_permutation(self):
         # Check if the lengths are equal
-        if len(self.str1) != len(self.str2):
+        if len(self.first_string) != len(self.second_string):
             return False
         
         # Sort both strings
-        sorted_str1 = sorted(self.str1)
-        sorted_str2 = sorted(self.str2)
+        sorted_first_string = sorted(self.first_string)
+        sorted_second_string = sorted(self.second_string)
         
         # Compare the sorted strings
-        return sorted_str1 == sorted_str2
+        return sorted_first_string == sorted_second_string
 
 
 # Example usage:
-str1 = str(input("Enter first string: "))
-str2= str(input("Enter second string: "))
+# I used inputs, because there are no given strings in the book
+first_string = str(input("Enter first string: "))
+second_string= str(input("Enter second string: "))
 
-result = Solution(str1, str2)
+result = PermutationChecker(first_string, second_string)
 
 if result.is_permutation():
-    print(f'"{str1}" is a permutation of "{str2}".')
+    print(f'"{first_string}" is a permutation of "{second_string}".')
 else:
-    print(f'"{str1}" is not a permutation of "{str2}".')
+    print(f'"{first_string}" is not a permutation of "{second_string}".')
+
