812d8e56d5612354cf85a182c7a402352db20efb
Elthar <100107294+Elthar@users.noreply.github.com>
2023-10-15 09:48:00 +0600

    4. new urlify.py

--- urlify.py
+++ urlify.py
@@ -1,22 +1,21 @@
 # Class which will make URL link from string by changing eac spaces to "%20"
-class Solution:
+class Urlifier:
     # initialize variables
-    def __init__(self, str1, str_length):
-        self.str1 = str1
+    def __init__(self, my_string, str_length):
+        self.my_string = my_string
         self.str_length = str_length
     
     def urlify(self):
         # replace spaces to %20
-        str_list = list(self.str1)
+        str_list = list(self.my_string)
         for i in range(self.str_length -1, -1, -1):
             if str_list[i] == ' ':
-                str_list[i+2] = '0'
-                str_list[i+1] = '2'
-                str_list[i] = '%'
+                str_list[i] = '%20'
         return ''.join(str_list)
 # Example usage
-str1 = str(input())
-str_length = len(str1)
-urlifier = Solution(str1, str_length)
+my_string = 'Mr 3ohn Smith'
+str_length = len(my_string)
+urlifier = Urlifier(my_string, str_length)
 result = urlifier.urlify()
 print(result)
+
