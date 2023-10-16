e1b55d568aa433bdfd8fea1a704897ae74779f3d
Elthar <100107294+Elthar@users.noreply.github.com>
2023-10-16 12:24:36 +0600

    5. new is_one_away.py

--- is_one_away.py
+++ is_one_away.py
@@ -1,36 +1,40 @@
-# Class, which checks if we can get second string by editing first string with only one move and vice versa 
-# insert a character, remove a character, or replace a character - each are considered as a one move. 
-# True - if we can get strx1 by editing strx2 only with one move, False - if we have to edit strx1 two or more times to get strx2
+# Class for comparing two strings to determine if they are one edit (insert, remove, or replace) away from each other.
+# It provides a method to check if two strings meet the "One Away" criteria.
 class Solution:
     # Initialize variables
-    def __init__(self, str1, str2):
-        self.str1 = str1
-        self.str2 = str2
+    def __init__(self, first_string, second_string):
+        self.first_string = first_string
+        self.second_string = second_string
     def is_one_away(self):
         # turning strings into lists
-        list1 = [str(i) for i in self.str1]
-        list2 = [str(j) for j in self.str2]
+        my_first_list = [str(i) for i in self.first_string]
+        my_second_list = [str(j) for j in self.second_string]
 
         res = []
         res2 = []
-        # Check if difference between two lists is 2 or more
-        for x in list1:
-            if x not in list2:
-                res.append(x)
-        for x in list2:
-            if x not in list1:
-                res2.append(x)
-        if len(list1) - len(list2) > 1 or len(list2) - len(list > 1):
+        # Check if lists were edited 2 or more times
+        for i in my_first_list:
+            if i not in my_second_list:
+                res.append(i)
+        for i in my_second_list:
+            if i not in my_first_list:
+                res2.append(i)
+
+        if len(my_first_list) - len(my_second_list) > 1 or len(my_second_list) - len(my_first_list) > 1:
             return False
         elif len(res) > 1 or len(res2) > 1:
             return False
         else:
             return True
 
-# Input 2 strings and check if it was edited once or more times
-str1 = str(input('Enter first string: '))
-str2 = str(input('Enter second string: '))
+# Example usage
 
-result = Solution(str1, str2)
-final_result = result.is_one_away()
-print(final_result)
+result = Solution("pale", "pie")
+print("pale, pie ->", result.is_one_away()) # False (we should replace 'i' by 'l' and add 'a' in second string, but in the book its true, idk why)
+result = Solution("pales", "pale")
+print("pales, pale ->",result.is_one_away()) # True (remove 's')
+result = Solution("pale", "bale")
+print("pale, bale ->",result.is_one_away()) # True (replace 'p' with 'b')
+result = Solution("pale", "bake")
+print("pale, bake ->",result.is_one_away()) # False (two edits needed)
+
