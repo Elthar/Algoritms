e779494c197ebc8bfee8f668f45da6476f348b1b
Elthar <100107294+Elthar@users.noreply.github.com>
2023-10-16 12:37:29 +0600

    9. new isSubstract.py

--- isSubstract.py
+++ isSubstract.py
@@ -1,29 +1,29 @@
-# In this class we check if str2 is a substraction of str1
+# In this claboth_strings we check if second_string is a substraction of first_string
 class Solution:
-    def __init__(self, str1):
-        self.str1 = str1
+    def __init__(self, first_string):
+        self.first_string = first_string
 
-    def is_substring(self, str1, str2):
-        # Placeholder function for checking if str2 is a substring of str1
+    def is_substring(self, first_string, second_string):
+        # Placeholder function for checking if second_string is a substring of first_string
         # We can replace this with our own implementation or use Python's 'in' operator
-        return str2 in str1
+        return second_string in first_string
 
-    def is_rotation(self, str2):
-        if len(self.str1) == len(str2) and len(self.str1) > 0:
-            # Concatenate self.str1 with itself to create a string containing all rotations
-            ss = self.str1 + self.str1
+    def is_rotation(self, second_string):
+        if len(self.first_string) == len(second_string) and len(self.first_string) > 0:
+            # Concatenate self.first_string with itself to create a string containing all rotations
+            both_strings = self.first_string + self.first_string
 
-            # Check if str2 is a substring of ss
-            return self.is_substring(ss, str2)
+            # Check if second_string is a substring of both_strings
+            return self.is_substring(both_strings, second_string)
 
         return False
 
 # Example usage
 
-str1 = str(input("Enter first string: "))
-str2 = str(input("Enter second string: "))
+first_string =  "waterbottle"
+second_string = "erbottlewat"
 
-rotator = Solution(str1)
-result = rotator.is_rotation(str2)
-# Output should be True if str2 is substraction of str1, otherwise it returns False
+rotator = Solution(first_string)
+result = rotator.is_rotation(second_string)
+# Output should be True if second_string is substraction of first_string, otherwise it returns False
 print(result)  
