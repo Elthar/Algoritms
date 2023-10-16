e22535630e0d1c3f425b59933b4830fdec44d2e2
Elthar <100107294+Elthar@users.noreply.github.com>
2023-10-16 12:29:23 +0600

    6. new StringCompressor.py

--- StringCompressor.py
+++ StringCompressor.py
@@ -1,7 +1,7 @@
 # Class for compressing strings
 class Solution:
-    def __init__(self, str1):
-        self.str1 = str1
+    def __init__(self, string):
+        self.string = string
 
     def compress(self):
         # Compresses the input string using the counts of repeated characters.
@@ -12,24 +12,24 @@ class Solution:
         count = 1
 
         # Check if the current character is the same as the previous character
-        for i in range(1, len(self.str1)):
+        for i in range(1, len(self.string)):
 
-            if self.str1[i] == self.str1[i - 1]:
+            if self.string[i] == self.string[i - 1]:
                 count += 1
             else:
                 # If the current character is different, add the previous character and its count to the compressed string
-                compressed_string += self.str1[i - 1] + str(count)
+                compressed_string += self.string[i - 1] + str(count)
                 count = 1
 
-        compressed_string += self.str1[-1] + str(count)
+        compressed_string += self.string[-1] + str(count)
         # Check if the compressed string is shorter than the original string
-        if len(compressed_string) >= len(self.str1):
-            return self.str1
+        if len(compressed_string) >= len(self.string):
+            return self.string
         else:
             return compressed_string
 
 # Example usage:
-input_str = str(input())
-compressor = Solution(input_str)
+
+compressor = Solution('aabcccccaaa')
 compressed_result = compressor.compress()
 print(compressed_result) 
